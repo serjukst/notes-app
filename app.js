3d28725475f0958f37bca8ce9f243e5c27772f51
Serhii Stepanchuk <serjukst@gmail.com>
2020-11-01 22:18:46 +0200

    make react build and connect with nodejs

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
+app.use(express.static('client/build'));
 
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/notes', require('./routes/notes.routes'));
 app.use('/api/users', require('./routes/user.routes'));
+
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const PORT = process.env.PORT || config.get('port') || 5000;
 
---
+++ client/build/precache-manifest.425b75c5a2ae904de6b0156ccc219185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e479d0e4a7fc702e9c9fba1a5927b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075b279a024c02e72ea5",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "add34c12c02d5acd892a",
+    "url": "/static/css/main.c81e8711.chunk.css"
+  },
+  {
+    "revision": "075b279a024c02e72ea5",
+    "url": "/static/js/2.c40ac413.chunk.js"
+  },
+  {
+    "revision": "e66fb051b8f7949fba83cb65b359d399",
+    "url": "/static/js/2.c40ac413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add34c12c02d5acd892a",
+    "url": "/static/js/main.a13a9150.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
